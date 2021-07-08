ea759b230cede90054700a00c5488d69a75f0c65
OrayKT <oraykt@gmail.com>
2021-07-08 14:05:55 +0200

    User

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
+import User from './User'
 
 
 export class Main extends Component {
   render() {
     return (
       <div className="container">
-        <Navbar/>
+        <Navbar />
+        <User />
       </div>
     )
   }
