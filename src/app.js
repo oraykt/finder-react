60b2ec8040fa97a48f01873a2f1ec15160d9af95
OrayKT <oraykt@gmail.com>
2021-07-08 13:25:47 +0200

    Hello World!

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/main.scss'
+import Main from './components/Main'
 
 
-
-ReactDOM.render(React.createElement('h1', {}, 'Hello World!' ), document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
