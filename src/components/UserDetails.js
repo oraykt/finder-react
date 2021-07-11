24d4eed0a62ba51a355ceea5a05772ba8d9f8c31
OrayKT <oraykt@gmail.com>
2021-07-11 19:26:03 +0200

    React.useEffect()

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,56 +1,57 @@
-import React, { Component, Fragment } from 'react'
+import React, { useEffect, Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import Loading from './Loading'
 import Repos from './Repos'
 
-class UserDetails extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login)
-    this.props.getUserRepos(this.props.match.params.login)
-  }
+const UserDetails = ({getUser, getUserRepos, match, loading, user, repos}) => {
+  
+  useEffect(() => {
+    getUser(match.params.login)
+    getUserRepos(match.params.login)
+  },[])
 
-  render() {
-    const { loading, user, repos } = this.props
-    if(loading) return <Loading />
-    return (
-      <div className="container mt-3">
-        <div className="row">
-          <div className="col-md-3">
-            <div className="card">
-              <img src={user.avatar_url} className="card-img-top" />
-              <div className="card-body">
-                <p className="card-text">{user.name}</p>
-                <p><i className="fa fa-map-marker-alt"></i> {user.location}</p>
-                <p>
-                  <Link to={{pathname:user.html_url}} target="_blank">Github Profile</Link>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-9">
-            <div className="card">
-              <div className="card-body">
-                {
-                  user.bio && <Fragment> <h3>About</h3> <p>{user.bio}</p></Fragment>
-                }
-                {
-                  user.blog && <Fragment> <h3>Blog</h3> <p>{user.blog}</p></Fragment>
-                }
-                <div>
-                  <span className="badge badge-primary m-1"> Followers: {user.followers}</span>
-                  <span className="badge badge-info m-1"> Following: {user.following}</span>
-                  <span className="badge badge-success m-1"> Repo: {user.public_repos}</span>
-                </div>
-              </div>
-              <ul className="list-group list-group-flush">
-                <Repos repos={repos}/>
-              </ul>
+  if (loading) return <Loading />
+  
+  const { avatar_url, html_url, location, name, bio, blog, followers, following, public_repos } = user
+  
+  return (
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-md-3">
+          <div className="card">
+            <img src={avatar_url} className="card-img-top" />
+            <div className="card-body">
+              <p className="card-text">{name}</p>
+              <p><i className="fa fa-map-marker-alt"></i> {location}</p>
+              <p>
+                <Link to={{pathname:html_url}} target="_blank">Github Profile</Link>
+              </p>
             </div>
           </div>
         </div>
+        <div className="col-md-9">
+          <div className="card">
+            <div className="card-body">
+              {
+                bio && <Fragment> <h3>About</h3> <p>{bio}</p></Fragment>
+              }
+              {
+                blog && <Fragment> <h3>Blog</h3> <p>{blog}</p></Fragment>
+              }
+              <div>
+                <span className="badge badge-primary m-1"> Followers: {followers}</span>
+                <span className="badge badge-info m-1"> Following: {following}</span>
+                <span className="badge badge-success m-1"> Repo: {public_repos}</span>
+              </div>
+            </div>
+            <ul className="list-group list-group-flush">
+              <Repos repos={repos}/>
+            </ul>
+          </div>
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default UserDetails
